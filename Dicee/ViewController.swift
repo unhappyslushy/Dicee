e07cc45270e11ea48f4416aef7a9dc1129c125b5
UnHappySlushy <jakobt2@learn.cssd.ab.ca>
2018-09-12 14:37:58 -0600

    Linked the dice and roll into code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+   
+    }
 
 }
 
