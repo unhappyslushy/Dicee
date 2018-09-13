4b078b344b8cf33b8f994e73702f22a11fcd4eb4
UnHappySlushy <jakobt2@learn.cssd.ab.ca>
2018-09-13 14:49:56 -0600

    Finished

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
+    let diceArray = ["dice1", "dice2","dice3","dice4","dice5","dice6"]
+    
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -34,6 +37,10 @@ class ViewController: UIViewController {
         
         print(randomDiceIndex1)
         print(randomDiceIndex2)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
     }
 
 }
